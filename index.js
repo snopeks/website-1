2f85b341f15371484dfaf20a6a8c0d93c4e83be4
snopeks <creativesnopeks@gmail.com>
2018-05-20 15:17:51 -0700

    add login and signup forms

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ $(function(){
   //show the signup form
   $("#signupLink").on('click', function(e){
     e.preventDefault();
-    console.log(e);
-    $("#signup").empty();
-    $("#signup").append(`
-      <form id="signupForm" action="POST">
-        <label for="userEmail">Email:</label>
-        <input id="userEmail" type="text" placeholder="enter your email" required>
-        <label for="userPassword" required>Password:</label>
-        <input type="password" placeholder="choose a password">
-        <input type="submit">
-      </form>
-      `)
+    if($("#signuplogin").has("form").length){
+      $("#signuplogin").empty();
+    } else {
+      $("#signuplogin").empty();
+      $("#signuplogin").append(`
+        <form id="signupForm" action="POST">
+          <h3>Sign up to create your own posts!</h3>
+          <label for="userEmail">Email:</label>
+          <input id="userEmail" type="email" placeholder="enter your email" required>
+          <label for="userPassword">Password:</label>
+          <input type="password" placeholder="choose a password" required>
+          <input type="Sign up">
+        </form>
+        `)
+    }
+  })
+  $("#loginLink").on('click', function(e){
+    e.preventDefault();
+    if($("#signuplogin").has('form').length){
+      $("#signuplogin").empty();
+    } else {
+      $("#signuplogin").empty();
+      $("#signuplogin").append(`
+        <form id="loginForm" action="POST">
+          <h3>Login to your account</h3>
+          <label for="userEmail"> Email: </label>
+          <input id="userEmail" type="email" required>
+          <label for="userPassword">Password: </label>
+          <input type="password" placeholder="enter your password" required>
+        </form>
+        `)
+    }
   })
 });
